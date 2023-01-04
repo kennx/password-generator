6477c9f3a0aaa364ff052da0dbc7ede551926bd8
zhangjian <zhangxjian@me.com>
2023-01-04 10:11:15 +0800

    update

---
+++ src/constants/characters.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-useless-escape */
+export const UPPER_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const LOWER_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz';
+export const NUMBER_CHARACTERS = '1234567890';
+export const SPECIAL_CHARACTERS = `~!@#$%^&*()-=_+[]\{}|;':",./<>?`;

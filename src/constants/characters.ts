416ca029af545cc7bea411f29ec06d9c9ebdbf30
zhangjian <zhangxjian@me.com>
2023-01-04 09:41:47 +0800

    update

--- src/constants/characters.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-useless-escape */
-export const UPPER_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-export const LOWER_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz';
-export const NUMBER_CHARACTERS = '1234567890';
-export const SPECIAL_CHARACTERS = `~!@#$%^&*()-=_+[]\{}|;':",./<>?`;
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import '../app.css';
-</script>
-
-<svelte:head>
-  <title>Password Generator</title>
-</svelte:head>
-<slot />
